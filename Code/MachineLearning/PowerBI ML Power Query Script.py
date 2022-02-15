ffae04ed2cdf2e831742632558ff40bd8d524817
jlb14n <94406601+jlb14n@users.noreply.github.com>
2022-02-15 14:18:11 -0500

    Updated with correct scaling

--- Code/MachineLearning/PowerBI ML Power Query Script.py
+++ Code/MachineLearning/PowerBI ML Power Query Script.py
@@ -4,9 +4,12 @@ import pandas as pd
 from joblib import load
 from sklearn.impute import KNNImputer
 from sklearn.preprocessing import StandardScaler
+import json
 
 loaded_model = load('C:/Users/jbartell/Desktop/Dev10/M11 Capstone/ML/random_forest_model_scaled.model')
 subset_to_keep = ["SO2","OZONE","PM25"]
+with open("C:/Users/jbartell/Desktop/Dev10/M11 Capstone/ML/X_train_scaled_dropnulls.json") as f:
+    scaler_dict = json.load(f)
 
 df = pd.DataFrame(dataset)
 columns=df.columns
@@ -19,5 +22,7 @@ for param in params:
     df.drop([f"{param}_METHOD_ID",f"{param}_METRIC_ID",f"{param}_UNITS_ID"],axis=1,inplace=True,errors="ignore")
     if param not in subset_to_keep:
         df.drop([f"{param}_MEAN",f"{param}_1STMAX",f"{param}_99PERC",f"{param}_STD",f"{param}_2NDMAX"],axis=1,inplace=True,errors="ignore")
-df = StandardScaler().fit_transform(df)
+for col in df.columns:
+    df[col] = df[col].apply(lambda x: (x-scaler_dict['Mean'][col])/(scaler_dict['Var'][col]**0.5))
 dataset['predict']=loaded_model.predict(df)
+del df
